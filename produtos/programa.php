ebcdac1a2a1a4cc6e3b5e2e9133a6bcead2faf48
alexbacke <atilacal@hotmail.com>
2013-12-18 11:59:05 -0200

    Create programa.php

---
+++ produtos/programa.php
@@ -0,0 +1,15 @@
+<?php
+$codigo = 0;
+
+$codigo = $_POST["codigo"];
+
+if ($codigo == 1){
+ 	echo"caneta 1.20";
+}
+else if ($codigo == 2){
+	echo"lapis 0.80";
+}
+else if ($codigo == 3){
+	echo"caderno 4.50";
+} 
+?>
