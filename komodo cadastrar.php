31a01be30d4ef99827914b3bae3d93eb82c3292b
alexbacke <atilacal@hotmail.com>
2014-01-08 11:32:27 -0200

    Create komodo cadastrar.php

---
+++ komodo cadastrar.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once("menu.php");
+
+    session_start();
+    
+    $nome = $_REQUEST["nome"];
+    $sexo = $_REQUEST["sexo"];
+    
+    
+    if(!isset($_SESSION["cadastros"])) {
+        $_SESSION["cadastros"] = array();
+    }
+    
+    $pessoa = array("nome" => $nome, "sexo" => $sexo);
+    
+    
+    // array_push ela colocar o nome da pessoa a ser cadastrada no final da lista
+    array_push($_SESSION["cadastros"], $pessoa);
+    
+    echo "Cadastro efeituado com sucesso";
+?>
